14b372728ffdb491562f5bb65e05dd91e210fbd4
Alec Stanley <stanleyalec283@gmail.com>
2026-06-04 19:55:17 +1000

    progress

--- src/figure.cpp
+++ src/figure.cpp
@@ -35,8 +35,8 @@ void Figure::plot(const dynamic_array<double> &x, const dynamic_array<double> &y
         pt.markersize = _markersize;
 
         // Calculate and store screen coordinates (with rounding)
-        pt.pos.x = static_cast<int>(std::round(x_padding + (x[i] - xlim.x) / x_range * drawable_width));
-        pt.pos.y = static_cast<int>(std::round(dimensions.y - y_padding - (y[i] - ylim.y) / y_range * drawable_height));
+        pt.pos.x = static_cast<int>(std::round(pos.x + x_padding + (x[i] - xlim.x) / x_range * drawable_width));
+        pt.pos.y = static_cast<int>(std::round(pos.y + dimensions.y - y_padding - (y[i] - ylim.x) / y_range * drawable_height));
         pt.pos.z = 0.0;
 
         buffer.add(pt);
